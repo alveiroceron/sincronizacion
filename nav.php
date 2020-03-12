9d4c54fd920398ca2321aebc0a05584d7c3c19a1
Alveiro <alveiroceron@hotmail.com>
2020-03-12 14:00:52 -0500

    modificando localmente nav

---
+++ nav.php
@@ -0,0 +1,9 @@
+<nav>
+    <ol>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Quienes somos</a></li>
+        <li><a href="#">Nosotros</a></li>
+        <li><a href="#">Contacto</a></li>
+
+    </ol>
+</nav>
